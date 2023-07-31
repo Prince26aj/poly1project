fd0baac551cf78904bb588efee386df678b70906
Prince26aj <137023364+Prince26aj@users.noreply.github.com>
2023-07-31 18:15:39 +0530

    Update batchMint.js

--- scripts/batchMint.js
+++ scripts/batchMint.js
@@ -1,35 +1,26 @@
-// This script batch mints criceket ERC721A tokens.
-
-// Import required libraries
 const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-  // Get private key from env
+
   const privateKey = process.env.PRIVATE_KEY;
 
-  // The URL of the network provider
+ 
   const networkAddress =
     "https://eth-goerli.g.alchemy.com/v2/53q66jVbbz9nAJtQAk2MbQ3ZVNdJ3GEI";
-
-  // Create a provider using the URL
+  
   const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-  // Create a signer from the private key and provider
   const signer = new ethers.Wallet(privateKey, provider);
 
-  // Tthe address of the deployed contract
   const contractAddress = "0x1689cE9a48553bf3418a1e481959356b4246d7Ef";
 
-  // Get the contract factory and attach it to the signer
   const IndianNFT = await ethers.getContractFactory("Humans", signer);
   const contract = await IndianNFT.attach(contractAddress);
 
-  // Call the mint function on the contract to mint 5 tokens
   await contract.mint(5);
 
-  // Log a message to the console to indicate that the tokens have been minted
-  console.log("Minted 5 tokens");
+  console.log("Minted five tokens here");
 }
 
 main()
